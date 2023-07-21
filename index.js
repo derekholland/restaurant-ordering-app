7f8e750748ba7a5fe729b733447e174e06888ca6
Derek Holland <derekhollandwebdev@gmail.com>
2023-07-21 16:35:20 -0400

    UI Complete

---
+++ index.js
@@ -0,0 +1,3 @@
+// when the plus button is clicked it brings up the order total at the bottom. Any time any plus button is clicked it adds to the order, the option to remove an item from the order is also available at the bottom
+
+// when complete order button is clicked the payment modal pops up in the center of the screen
